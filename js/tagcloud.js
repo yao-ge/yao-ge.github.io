57a996609073ad7c4c460c21e43a50e3dd40cfcc
yao-ge <yaoge2016@gmail.com>
2021-12-29 12:31:37 +0800

    add post vim configure

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -15,7 +15,7 @@
 
      try {
          TagCanvas.textFont = 'Trebuchet MS, Helvetica';
-         TagCanvas.textColour = '#555';
+         TagCanvas.textColour = '#333';
          TagCanvas.textHeight = 25;
          TagCanvas.outlineColour = '#E2E1D1';
          TagCanvas.maxSpeed = 0.1;
